c9c06d08b6fd0baca3580e7d72d593711739c12b
AidanZealley <aidan@zealley.com>
2026-05-02 23:25:29 +0100

    Move Web Bluetooth globals to .d.ts and include in tsconfig
    - Replace `globals.ts` ambient declarations with `globals.d.ts`
    - Update `packages/trainer-io` tsconfig to include `.d.ts` files

---
+++ packages/trainer-io/src/transports/bluetooth/web-bluetooth/globals.d.ts
@@ -0,0 +1,61 @@
+type BluetoothServiceUUID = string | number
+type BluetoothCharacteristicUUID = string | number
+
+type BluetoothLEScanFilter = {
+  services?: Array<BluetoothServiceUUID>
+  name?: string
+  namePrefix?: string
+}
+
+type RequestDeviceOptions =
+  | {
+      filters: Array<BluetoothLEScanFilter>
+      optionalServices?: Array<BluetoothServiceUUID>
+      exclusionFilters?: Array<BluetoothLEScanFilter>
+      acceptAllDevices?: false
+    }
+  | {
+      acceptAllDevices: true
+      optionalServices?: Array<BluetoothServiceUUID>
+      exclusionFilters?: Array<BluetoothLEScanFilter>
+      filters?: never
+    }
+
+interface Bluetooth {
+  requestDevice: (options: RequestDeviceOptions) => Promise<BluetoothDevice>
+}
+
+interface Navigator {
+  bluetooth?: Bluetooth
+}
+
+interface BluetoothDevice extends EventTarget {
+  readonly id?: string
+  readonly name?: string
+  readonly gatt?: BluetoothRemoteGATTServer
+}
+
+interface BluetoothRemoteGATTServer {
+  readonly connected: boolean
+  connect: () => Promise<BluetoothRemoteGATTServer>
+  disconnect: () => void
+  getPrimaryService: (
+    service: BluetoothServiceUUID
+  ) => Promise<BluetoothRemoteGATTService>
+}
+
+interface BluetoothRemoteGATTService {
+  getCharacteristic: (
+    characteristic: BluetoothCharacteristicUUID
+  ) => Promise<BluetoothRemoteGATTCharacteristic>
+}
+
+interface BluetoothRemoteGATTCharacteristic extends EventTarget {
+  readonly uuid: string
+  readonly value?: DataView | null
+  readValue: () => Promise<DataView>
+  writeValue: (value: BufferSource) => Promise<void>
+  writeValueWithResponse?: (value: BufferSource) => Promise<void>
+  startNotifications: () => Promise<BluetoothRemoteGATTCharacteristic>
+  stopNotifications?: () => Promise<BluetoothRemoteGATTCharacteristic>
+}
